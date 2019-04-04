f3477239de5d69add342ba175adfc00245d08449
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-04 03:19:13 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,7 @@ class School
   def add_student(name, grade)
     if roster[grade] == nil
       roster[grade] = []
-      roster << name
+      roster[grade].push(name)
     else
       roster[grade].push(name)
     end
