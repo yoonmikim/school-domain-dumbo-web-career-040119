637e28451c61d2f37c9cd02a57f94ae2d3ae0a21
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-04 03:27:16 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+class School 
+  attr_accessor :name, :roster
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(name, grade)
+    if roster[grade] == nil
+      roster[grade] = []
+      roster[grade].push(name)
+    else
+      roster[grade] << (name)
+    end
+  end
+  
+  def grade(n)
+    roster[n]
+  end
+  
+  def sort
+    sorted = roster.map { |k, v| roster[k] = roster[k].sort }
+   roster
+  end
+  
+end
