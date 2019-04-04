cfd042ba0bf52156b06965b611f0ba348c57a3df
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-04-04 03:17:55 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -8,20 +8,20 @@ class School
   end
   
   def add_student(name, grade)
-    if @roster[grade] == nil 
-      @roster[grade] = []
-      @roster << name
+    if roster[grade] == nil 
+      roster[grade] = []
+      roster << name
     else
-      @roster[grade] << name
+      roster[grade] << name
     end
   end
   
   def grade(n)
-    @roster[n]
+    roster[n]
   end
   
   def sort
-    @roster[grade].map { |i| i.sort }
+    roster[grade].map { |i| i.sort }
   end
   
 end
